23c733c6f50f7279d136ae4f285e3bb75b14ebfb
Unknown <cjalberto1503@gmail.com>
2018-03-18 19:41:22 -0400

    base de datos

---
+++ base de datos/atletismo_IDT.sql
@@ -0,0 +1,189 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 18-03-2018 a las 22:51:00
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `atletismo_IDT`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `atleta`
+--
+
+CREATE TABLE `atleta` (
+  `id` int(11) NOT NULL,
+  `primer_nombre` varchar(30) NOT NULL,
+  `segundo_nombre` varchar(30) NOT NULL,
+  `primer_apellido` varchar(30) NOT NULL,
+  `segundo_apellido` varchar(30) NOT NULL,
+  `cedula` int(11) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `id_club` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `club`
+--
+
+CREATE TABLE `club` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `competencia`
+--
+
+CREATE TABLE `competencia` (
+  `id` int(11) NOT NULL,
+  `nombre` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `competencia_categoria`
+--
+
+CREATE TABLE `competencia_categoria` (
+  `id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `lugar` varchar(255) NOT NULL,
+  `id_atleta` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `id_competencia` int(11) NOT NULL,
+  `tiempo` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `atleta`
+--
+ALTER TABLE `atleta`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `cedula_unique` (`cedula`),
+  ADD KEY `foreing_id_club` (`id_club`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_nombre` (`nombre`);
+
+--
+-- Indices de la tabla `club`
+--
+ALTER TABLE `club`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uique_nombre` (`nombre`);
+
+--
+-- Indices de la tabla `competencia`
+--
+ALTER TABLE `competencia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `competencia_categoria`
+--
+ALTER TABLE `competencia_categoria`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `foreing_id_atleta` (`id_atleta`),
+  ADD KEY `foreing_id_categoria` (`id_categoria`),
+  ADD KEY `foreing_id_competencia` (`id_competencia`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `atleta`
+--
+ALTER TABLE `atleta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `club`
+--
+ALTER TABLE `club`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `competencia`
+--
+ALTER TABLE `competencia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `competencia_categoria`
+--
+ALTER TABLE `competencia_categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `atleta`
+--
+ALTER TABLE `atleta`
+  ADD CONSTRAINT `foreing_id_club` FOREIGN KEY (`id_club`) REFERENCES `club` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `competencia_categoria`
+--
+ALTER TABLE `competencia_categoria`
+  ADD CONSTRAINT `foreing_id_atleta` FOREIGN KEY (`id_atleta`) REFERENCES `atleta` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `foreing_id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `foreing_id_competencia` FOREIGN KEY (`id_competencia`) REFERENCES `competencia` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
