0d431e70c1464af5e6138cb9d856fcbb1a6b1102
Unknown <cjalberto1503@gmail.com>
2018-03-29 19:33:33 -0400

    filtrar atletas

--- server/routes/historial.js
+++ server/routes/historial.js
@@ -20,7 +20,7 @@ router
 			promesa
 				.then((dataCompetencia) => {
 					return new Promise((resolve , reject) => {
-						conexion.query(`SELECT CONCAT(atl.primer_nombre , ' ' , atl.primer_apellido) as 'competidor', com_at.numero_atleta as 'numero' , SEC_TO_TIME(com_at.tiempo) as 'tiempo' FROM competencia_atleta com_at LEFT JOIN atleta atl ON com_at.id_atleta=atl.id WHERE com_at.id_competencia=${competencia_id} AND atl.sexo='${dataCompetencia.sexo}' ORDER BY tiempo ASC`, (err , rows) =>{
+						conexion.query(`SELECT CONCAT(atl.primer_nombre , ' ' , atl.primer_apellido) as 'competidor', com_at.numero_atleta as 'numero' , SEC_TO_TIME(com_at.tiempo) as 'tiempo' FROM competencia_atleta com_at LEFT JOIN atleta atl ON com_at.id_atleta=atl.id WHERE com_at.id_competencia=${competencia_id}  ORDER BY tiempo ASC`, (err , rows) =>{
 							(err) ? reject(new Error('Error al consultar la base de datos')) : resolve({dataCompetencia : dataCompetencia , dataCompetidores : rows})
 						})
 					})
@@ -33,9 +33,52 @@ router
 				})
 		})
 	})
-	.get('/historial/historial1', (req, res , next) => {
-		console.log('entro')
-		res.render('historial/historial1')
+	.post('/historial/filtrar-carrera', (req, res , next) => {
+		let text = req.body.text
+		req.getConnection((err , conexion) => {
+			if (err){
+				res.render('error', {mensaje : 'Error al conectarse a la base de datos' , code : 404})
+			}
+			conexion.query(`SELECT competencia.id , competencia.nombre as 'nombre' , competencia.fecha , competencia.hora , competencia.lugar , categoria.nombre as 'categoria' , categoria.sexo as 'sexo' FROM competencia LEFT JOIN categoria ON competencia.id_categoria=categoria.id WHERE finalizado = 1 AND competencia.nombre LIKE '%${text}%'` , (err , rows) =>{
+				if (err){
+					res.render('error', {mensaje : 'Error al consultar la base de datos' , code : 404})
+				}
+				res.render('historial', { Competencias: rows })
+			})
+		})
 	})
-
+	.post('/historial/filtrar-atletas', (req, res , next) => {
+		let text = req.body.text
+		let competencia_id = req.body.competencia_id
+		req.getConnection((err , conexion) => {
+			if (err){
+				res.render('error', {mensaje : 'Error al conectarse a la base de datos' , code : 404})
+			}
+			let promesa = new Promise((resolve , reject) => {
+				conexion.query(`SELECT com.nombre as 'nombreCompetencia' , DATE_FORMAT(com.fecha,'%d/%m/%Y') as 'fecha' , TIME(com.hora) as 'hora' , com.lugar as 'lugar' , cat.nombre as 'categoria'  , cat.sexo as 'sexo' FROM competencia com LEFT JOIN categoria cat ON com.id_categoria=cat.id where com.id = ?` , competencia_id, (err , rows) =>{
+					(err) ? reject(new Error('Error al consultar la base de datos')) : resolve(rows[0])
+				})
+			})
+			promesa
+				.then((dataCompetencia) => {
+					return new Promise((resolve , reject) => {
+						conexion.query(`SELECT CONCAT(atl.primer_nombre , ' ' , atl.primer_apellido) as 'competidor', com_at.numero_atleta as 'numero' , SEC_TO_TIME(com_at.tiempo) as 'tiempo' FROM competencia_atleta com_at LEFT JOIN atleta atl ON com_at.id_atleta=atl.id WHERE com_at.id_competencia=${competencia_id} AND (atl.primer_nombre LIKE '%${text}%' OR atl.primer_apellido LIKE '%${text}%' OR com_at.numero_atleta LIKE '%${text}%') ORDER BY tiempo ASC` , (err , rows) =>{
+							(err) ? reject(new Error('Error al consultar la base de datos')) : resolve({dataCompetencia : dataCompetencia , dataCompetidores : rows})
+						})
+					})
+				})
+				.then((data) => {
+					res.render('historial/detalle-competencia', data)
+				})
+				.catch((err) =>{
+					res.render('error', {mensaje : err.message , code : 404})
+				})
+			/*conexion.query(`SELECT CONCAT(atl.primer_nombre , ' ' , atl.primer_apellido) as 'competidor', com_at.numero_atleta as 'numero' , SEC_TO_TIME(com_at.tiempo) as 'tiempo' FROM competencia_atleta com_at LEFT JOIN atleta atl ON com_at.id_atleta=atl.id WHERE com_at.id_competencia=${competencia_id} AND (atl.primer_nombre LIKE '%${text}%' OR atl.primer_apellido LIKE '%${text}%' OR com_at.numero_atleta LIKE '%${text}%') ORDER BY tiempo ASC` , (err , rows) =>{
+				if (err){
+					res.render('error', {mensaje : 'Error al consultar la base de datos' , code : 404})
+				}
+				res.render('historial/detalle-competencia', { Competencias: rows })
+			})*/
+		})
+	})
 module.exports = router
