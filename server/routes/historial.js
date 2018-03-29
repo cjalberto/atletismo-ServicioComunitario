2dd2e5a6d3b53a72a6917f1cd6532422b7f4defd
Unknown <cjalberto1503@gmail.com>
2018-03-29 15:08:11 -0400

    Auto stash before merge of "master" and "origin/master"

--- server/routes/historial.js
+++ server/routes/historial.js
@@ -13,14 +13,14 @@ router
 				reject(new Error('Error al conectarse a la base de datos'))
 			}
 			let promesa = new Promise((resolve , reject) => {
-				conexion.query(`SELECT com.nombre as 'nombreCompetencia' , DATE_FORMAT(com.fecha,'%d/%m/%Y') as 'fecha' , TIME(com.hora) as 'hora' , com.lugar as 'lugar' , CONCAT(cat.nombre , '-' , cat.sexo) as 'categoria' FROM competencia com LEFT JOIN categoria cat ON com.id_categoria=cat.id where com.id = ?` , competencia_id, (err , rows) =>{
+				conexion.query(`SELECT com.nombre as 'nombreCompetencia' , DATE_FORMAT(com.fecha,'%d/%m/%Y') as 'fecha' , TIME(com.hora) as 'hora' , com.lugar as 'lugar' , cat.nombre as 'categoria'  , cat.sexo as 'sexo' FROM competencia com LEFT JOIN categoria cat ON com.id_categoria=cat.id where com.id = ?` , competencia_id, (err , rows) =>{
 					(err) ? reject(new Error('Error al consultar la base de datos')) : resolve(rows[0])
 				})
 			})
 			promesa
 				.then((dataCompetencia) => {
 					return new Promise((resolve , reject) => {
-						conexion.query(`SELECT CONCAT(atl.primer_nombre , ' ' , atl.primer_apellido) as 'competidor', com_at.numero_atleta as 'numero' , SEC_TO_TIME(com_at.tiempo) as 'tiempo' FROM competencia_atleta com_at LEFT JOIN atleta atl ON com_at.id_atleta=atl.id WHERE com_at.id_competencia=${competencia_id} AND atl.sexo='${dataCompetencia.categoria.split('-').slice(-1)[0]}' ORDER BY tiempo ASC`, (err , rows) =>{
+						conexion.query(`SELECT CONCAT(atl.primer_nombre , ' ' , atl.primer_apellido) as 'competidor', com_at.numero_atleta as 'numero' , SEC_TO_TIME(com_at.tiempo) as 'tiempo' FROM competencia_atleta com_at LEFT JOIN atleta atl ON com_at.id_atleta=atl.id WHERE com_at.id_competencia=${competencia_id} AND atl.sexo='${dataCompetencia.sexo}' ORDER BY tiempo ASC`, (err , rows) =>{
 							(err) ? reject(new Error('Error al consultar la base de datos')) : resolve({dataCompetencia : dataCompetencia , dataCompetidores : rows})
 						})
 					})
@@ -34,14 +34,8 @@ router
 		})
 	})
 	.get('/historial/historial1', (req, res , next) => {
-		req.getConnection((err , conexion) => {
-			if (err != null){
-				
-			}else{
-						res.render('historial/historial1')
-				
-			}
-		})
+		console.log('entro')
+		res.render('historial/historial1')
 	})
 
 module.exports = router
