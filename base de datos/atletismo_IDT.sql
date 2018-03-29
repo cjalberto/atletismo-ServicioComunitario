d4e1901e54c696bbfe7a16554b7f97c32d25f9b0
Unknown <cjalberto1503@gmail.com>
2018-03-29 12:37:07 -0400

    nueva base de datos

--- base de datos/atletismo_IDT.sql
+++ base de datos/atletismo_IDT.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 28-03-2018 a las 21:24:12
+-- Tiempo de generación: 29-03-2018 a las 18:36:39
 -- Versión del servidor: 10.1.31-MariaDB
 -- Versión de PHP: 7.2.3
 
@@ -37,8 +37,20 @@ CREATE TABLE `atleta` (
   `cedula` int(11) NOT NULL,
   `fecha_nacimiento` date NOT NULL,
   `id_club` int(11) NOT NULL,
-  `id_categoria` int(11) NOT NULL
+  `id_categoria` int(11) NOT NULL,
+  `sexo` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `atleta`
+--
+
+INSERT INTO `atleta` (`id`, `primer_nombre`, `segundo_nombre`, `primer_apellido`, `segundo_apellido`, `cedula`, `fecha_nacimiento`, `id_club`, `id_categoria`, `sexo`) VALUES
+(1, 'rbnfgn', 'fsfg', 'gsga', 'gdsg', 21312, '2018-03-14', 1, 1, 'M'),
+(2, 'geg', 'qwetgfsdg', 'gbdsgfd', 'gdsag', 3245, '2018-03-21', 2, 2, 'F'),
+(3, 'faweas', 'sfad', 'ghhdgh', 'dfasg', 35432, '2018-03-01', 3, 3, 'F'),
+(4, 'gerh', '5yuhjfn', 'nfncvzdfb', 'asgasg', 6362, '2018-03-13', 4, 4, 'M'),
+(5, 'hdsh', '', 'gdsfg', '', 5345, '2018-03-01', 1, 2, 'F');
 
 -- --------------------------------------------------------
 
@@ -52,6 +64,17 @@ CREATE TABLE `categoria` (
   `descripcion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'reqwer', 'sdafgsa'),
+(2, 'fwqfqw', 'fwqf'),
+(3, 'fwf', 'bvcvbbvd'),
+(4, 'hghjyj', 'liul'),
+(5, 'liutlr', 'erqwrtgw');
+
 -- --------------------------------------------------------
 
 --
@@ -64,6 +87,16 @@ CREATE TABLE `club` (
   `descripcion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `club`
+--
+
+INSERT INTO `club` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'sfa', 'fsfasf'),
+(2, 'fsdaf', 'qerwq'),
+(3, 'fw', 'rwqr'),
+(4, 'trtg', 'rwq');
+
 -- --------------------------------------------------------
 
 --
@@ -74,11 +107,19 @@ CREATE TABLE `competencia` (
   `id` int(11) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `fecha` date NOT NULL,
-  `hora` datetime NOT NULL,
+  `hora` time NOT NULL,
   `lugar` varchar(255) NOT NULL,
   `finalizado` tinyint(1) NOT NULL DEFAULT '0',
   `id_categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `competencia`
+--
+
+INSERT INTO `competencia` (`id`, `nombre`, `fecha`, `hora`, `lugar`, `finalizado`, `id_categoria`) VALUES
+(1, 'fsgg', '2018-03-15', '03:10:00', 'twtgsgas', 1, 1),
+(2, '344365', '2018-03-16', '12:15:00', 'fwgfewg', 1, 2);
 
 -- --------------------------------------------------------
 
@@ -93,6 +134,22 @@ CREATE TABLE `competencia_atleta` (
   `tiempo` double NOT NULL,
   `numero_atleta` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `competencia_atleta`
+--
+
+INSERT INTO `competencia_atleta` (`id`, `id_atleta`, `id_competencia`, `tiempo`, `numero_atleta`) VALUES
+(1, 1, 1, 300, 20),
+(2, 1, 2, 140, 40),
+(3, 2, 1, 204, 54),
+(4, 2, 2, 247, 82),
+(5, 3, 1, 52742, 2872),
+(6, 3, 2, 2572, 277),
+(7, 4, 1, 2727, 222),
+(8, 4, 2, 257, 788),
+(9, 5, 1, 2275.35, 288888),
+(10, 5, 2, 9898.1, 522);
 
 --
 -- Índices para tablas volcadas
@@ -144,31 +201,31 @@ ALTER TABLE `competencia_atleta`
 -- AUTO_INCREMENT de la tabla `atleta`
 --
 ALTER TABLE `atleta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `club`
 --
 ALTER TABLE `club`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `competencia`
 --
 ALTER TABLE `competencia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `competencia_atleta`
 --
 ALTER TABLE `competencia_atleta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
