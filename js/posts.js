4d7c3cf1e3c49cae4ed189c02a7faed4fe83aa14
Unknown <david.ortega@unet.edu.ve>
2018-04-01 15:32:02 -0400

    atleta/modificar

--- js/posts.js
+++ js/posts.js
@@ -190,3 +190,41 @@ $( "#crearAtleta" ).submit(function( event ) {
 	});
 
 });
+
+//POST editAtle ATLETA//
+
+$( "#modificarAtle" ).submit(function( event ) {
+ 
+  event.preventDefault();
+  console.log('editAtle Click');
+ 
+  var $form = $( this ),
+  datos = {}
+ datos.primer_nombre = $form.find( "input[name='primer_nombre']" ).val(),
+ datos.segundo_nombre = $form.find( "input[name='segundo_nombre']" ).val(),
+ datos.primer_apellido = $form.find( "input[name='primer_apellido']" ).val(),
+ datos.segundo_apellido = $form.find( "input[name='segundo_apellido']" ).val(),
+ datos.cedula = $form.find( "input[name='cedula']" ).val(),
+ datos.fecha_nacimiento = $form.find( "input[name='fecha_nacimiento']" ).val(),
+ datos.id_club = $form.find( "select[name='id_club']" ).val(),
+ datos.id_categoria = $form.find( "select[name='id_categoria']" ).val(),
+ datos.sexo = $form.find( "select[name='sexo']" ).val(),
+ urlpost = $form.attr( "action" );
+ 
+ 
+ $.ajax({
+  type: 'POST',
+  data: JSON.stringify(datos),
+     contentType: 'application/json',
+     url: urlpost,      
+     success: function(data) {
+         console.log(JSON.stringify(data));
+         if(data.mensaje=='acept'){
+          console.log("aceptoooooo");
+          alert("Atleta "+datos.primer_nombre+" Modificado");
+          location.href = "/gestionar/atleta";
+         }else{
+          alert("ERROR");
+         }}});
+ 
+});
