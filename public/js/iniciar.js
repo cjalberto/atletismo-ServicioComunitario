a0a95e9c5911ff49bcab09722a4e540cc67c9b66
Unknown <david.ortega@unet.edu.ve>
2018-04-01 10:23:06 -0400

    detalles

--- public/js/iniciar.js
+++ public/js/iniciar.js
@@ -101,37 +101,31 @@ document.onkeypress = info;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	
 var table = $('#Tablecompe').DataTable({
-	scrollCollapse: true,
-
-	"language": 
-	{
-    "sProcessing":     "Procesando...",
+ scrollCollapse: true,
+ 
+ "language": 
+ {
     "sLengthMenu":     "Mostrar _MENU_ registros",
     "sZeroRecords":    "No se encontraron resultados",
-    "sEmptyTable":     "Ningún dato disponible en esta tabla",
-    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-    "sInfoPostFix":    "",
     "sSearch":         "Buscar:",
-    "sUrl":            "",
-    "sInfoThousands":  ",",
-    "sLoadingRecords": "Cargando...",
-    "oPaginate": {
-        "sFirst":    "Primero",
-        "sLast":     "Último",
-        "sNext":     "Siguiente",
-        "sPrevious": "Anterior"
-    },
     "oAria": {
         
         "sSortDescending": ": Activar para ordenar la columna de manera descendente"
     },
-	},
+ },
      "bPaginate": false,
       "bInfo": false  
-			
+   
 });
-
+ 
+   $('#Tablecompe tbody').on('click', 'tr', function () {
+   	var counts = table.rows('.selected').data().length;
+   	
+      $(this).toggleClass('selected');  
+      var nomCompe= $('.selected').children('.nombreCompetencia').text();
+   
+   
+ 
+   });
+   
